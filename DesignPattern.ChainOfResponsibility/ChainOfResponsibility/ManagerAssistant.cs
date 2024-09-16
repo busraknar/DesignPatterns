e2c3d4d2ae49414a54ca242abfa8fce4a7af92ec
busraknar <b.ertas91@gmail.com>
2024-09-17 00:07:43 +0300

    ManagerAssistant class added

---
+++ DesignPattern.ChainOfResponsibility/ChainOfResponsibility/ManagerAssistant.cs
@@ -0,0 +1,13 @@
+﻿using DesignPattern.ChainOfResponsibility.Models;
+
+namespace DesignPattern.ChainOfResponsibility.ChainOfResponsibility
+{
+    public class ManagerAssistant : Employee
+
+    {
+        public override void ProcessRequest(CustomerProcessViewModel req)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
